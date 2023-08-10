3cd6c09a7e41023f29afea2b69bfb1637a64c3ce
村雨遥 <747731461@qq.com>
2023-08-10 17:02:54 +0800

    🎉 初始化

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,62 @@
+import { type DefaultTheme, defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/weekly/',
+  title: "村雨遥的好物周刊",
+  description: "村雨遥的好物周刊，记录每周看到的有价值的信息，主要针对计算机领域，包含项目、插件、资源、网站、软件等方面内容，每周五发布。",
+
+  lastUpdated: true,
+  cleanUrls: true,
+
+  head: [
+    ['link', { rel: 'icon', href: '/site/logo.png' }]
+  ],
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: { src: '/site/logo.png' },
+
+    editLink: {
+      pattern: 'https://github.com/cunyu1943/weekly/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+
+    search: {
+      provider: 'local'
+    },
+
+    footer: {
+      // message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023 - present <a href="https://github.com/cunyu1943">村雨遥</a>'
+    },
+
+    nav: nav(),
+
+    sidebar: {
+      '/2023/': { base: '/2023/', items: sidebarCurrent() }
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/cunyu1943/weekly' }
+    ]
+  }
+})
+
+function nav(): DefaultTheme.NavItem[] {
+  return [
+    { text: '主页', link: '/' },
+    { text: '2023', link: '/2023/0415-0421' }
+  ]
+}
+
+function sidebarCurrent(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: '2023 年刊',
+      collapsed: false,
+      items: [
+        { text: '周刊#1（0415-0421）', link: '0415-0421' },
+      ]
+    }]
+}
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  # name: "Weekly"
+  text: "村雨遥的好物周刊"
+  # tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: 创刊寄语
+      link: /markdown-examples
+    - theme: alt
+      text: 最新年刊
+      link: /2023/2
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
