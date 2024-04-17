6b7ee903872ce8fed33e183f53682ff8b32ce2c6
村雨遥 <747731461@qq.com>
2024-04-17 23:15:37 +0800

    ✨ feat(站点): 主题更新及定制化

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -44,21 +44,24 @@ export default defineConfig({
     search: {
       provider: 'local',
       options: {
-        detailedView: true,
-        miniSearch: {
-          // https://lucaong.github.io/minisearch/modules/MiniSearch.html
-          options: {
-            tokenize
-          },
-          searchOptions: {
-            combineWith: "AND",
-            fuzzy: 0.1,
-            prefix: true,
-            boost: {
-              title: 4,
-              text: 2,
+        
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换'
+                },
+              },
             },
-          }
+          },
         },
       },
     },
