b8c830154f1e4d28d42b2e62243df93428977eb4
村雨遥 <747731461@qq.com>
2024-08-07 19:17:14 +0800

    🪡 update

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -2,6 +2,6 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
     { text: ' 主页', link: '/' },
-    { text: ' 2024 年刊', link: '/2024/' },
-    { text: '2023 年刊', link: '/2023/' }
+    { text: ' 2024 年刊', link: '/2024/0101-0105' },
+    { text: '2023 年刊', link: '/2023/0415-0421' }
 ]
