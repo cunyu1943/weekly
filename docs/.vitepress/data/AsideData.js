894b92d046ec9b5f23c42e83025e3c1a97924629
村雨遥 <747731461@qq.com>
2024-08-12 23:01:58 +0800

    ✈️ 更新

---
+++ docs/.vitepress/data/AsideData.js
@@ -0,0 +1,23 @@
+// .vitepress/data/AsideData.ts
+
+export const Aside_Data = [
+    {
+        link: 'https://yuque.com/cunyu1943',
+        icon: 'https://cunyu1943.github.com/weekly/icon/yuque.svg',
+        Activity: '注册填邀请码即送30天会员',
+        info1: '邀请码 VUFNKN',
+        // info2: '年付 64折 优惠码  2461880'
+    },
+    // {
+    //     link: 'https://xx.theojs.cn/',
+    //     icon: 'https://i.theojs.cn/avatar.png',
+    //     name: '玄学宝典',
+    //     hide1: '传世经典著作',
+    //     hide2: '山医命相卜'
+    // },
+    // {
+    //     link: 'https://github.com/sponsors/cunyu1943',
+    //     icon: 'https://doc.theojs.cn/爱心.png',
+    //     name: '为我赞助'
+    // }
+]
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,16 +2,21 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style/index.css'
+import { DocBox, DocLinks, DocBoxCube, HomeUnderline, DocAsideLogo } from '@theo-messi/tm-fe'
+import { Aside_Data } from '../data/AsideData'
 
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'aside-ads-before': () => h(DocAsideLogo, { Aside_Data })
     })
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
+  enhanceApp: ({ app }) => {
+    app.component('Home', HomeUnderline)
+    app.component('Box', DocBox)
+    app.component('Links', DocLinks)
+    app.component('BoxCube', DocBoxCube)
   }
 }
