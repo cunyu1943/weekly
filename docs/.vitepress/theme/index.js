5e60c45aebca45cc9ce33d9f8388ec3b18abe69c
村雨遥 <747731461@qq.com>
2024-08-30 19:01:25 +0800

    🎈 插件更新

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,23 +2,23 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style/index.css'
-import { DocBox, DocLinks, DocBoxCube, HomeUnderline, DocAsideLogo } from '@theo-messi/tm-fe'
 import { Aside_Data } from '../data/AsideData'
 // theme/index.ts
-
 import '@theojs/lumen/theme'
+import { DocAsideLogo, Announcement, HomeUnderline } from '@theojs/lumen'
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
+      'home-hero-info-before': () => h(Announcement),
       'aside-outline-before': () => h(DocAsideLogo, { Aside_Data })
     })
   },
   enhanceApp: ({ app }) => {
     app.component('Home', HomeUnderline)
-    app.component('Box', DocBox)
-    app.component('Links', DocLinks)
-    app.component('BoxCube', DocBoxCube)
+    // app.component('Box', DocBox)
+    // app.component('Links', DocLinks)
+    // app.component('BoxCube', DocBoxCube)
   }
 }
