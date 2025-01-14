2974a3e6acb92b20b5c3c15a7987ebf98342147e
村雨遥 <747731461@qq.com>
2025-01-14 18:40:45 +0800

    ✨ feat(全站): 添加打赏

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,15 +1,15 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-    { text: ' 主页', link: '/' },
-    { text: ' 2025 年刊', link: '/2025/1228-0103' },
+    { text: '🏠 主页', link: '/' },
+    { text: '🐍 2025 年刊', link: '/2025/1228-0103' },
     {
-        text: '往昔留档',
+        text: '🗃️ 往昔留档',
         items: [
             { text: '🐉 2024 年刊', link: '/archive/2024/0101-0105' },
             { text: '🐇 2023 年刊', link: '/archive/2023/0415-0421' },
         ]
     },
-    { text: '', link: '/archive/2023/0415-0421' },
-    { text: '超大流量卡', link: 'https://ym.ksjhaoka.com/?s=Ltxbbs9W330131' }
+    { text: '💰 打赏', link: '/reward' },
+    { text: '💳 超大流量卡', link: 'https://ym.ksjhaoka.com/?s=Ltxbbs9W330131' }
 ]
