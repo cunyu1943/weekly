01138038c0caebe93d846dc55d7f91180d506f12
村雨遥 <747731461@qq.com>
2025-02-07 22:55:32 +0800

    chore: update npm dependency to latest stable version

--- docs/.vitepress/data/AsideData.js
+++ docs/.vitepress/data/AsideData.js
@@ -1,20 +1,20 @@
-// .vitepress/data/AsideData.ts
+// import type { AsideItem } from '@theojs/lumen'
 
 export const Aside_Data = [
     {
         link: 'https://ym.ksjhaoka.com/?s=Ltxbbs9W330131',
-        icon: 'https://cunyu1943.github.io/weekly/icon/sim.svg',
-        Activity: '点击办理超大流量卡',
+        image: 'https://cunyu1943.github.io/weekly/icon/sim.svg',
+        activity: '办理超低资费超大流量卡',
         info1: '官方正规卡号',
         // info2: '年付 64折 优惠码  2461880'
     },
     {
         link: 'https://yuque.com/cunyu1943',
-        icon: 'https://cunyu1943.github.io/weekly/icon/yuque.svg',
-        Activity: '注册填邀请码即送30天会员',
+        image: 'https://cunyu1943.github.io/weekly/icon/yuque.svg',
+        activity: '填写邀请码即送30天会员',
         info1: '邀请码 VUFNKN',
         // info2: '年付 64折 优惠码  2461880'
-    },
+    }
     // {
     //     link: 'https://xx.theojs.cn/',
     //     icon: 'https://i.theojs.cn/avatar.png',
