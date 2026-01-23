bca1626e85a50005b059f4259b97ee76aa3e7282
村雨遥 <cunyu1024@gmail.com>
2026-01-23 14:31:37 +0800

    docs: 更新年刊第140期
    - 在侧边栏配置中添加第140期链接并调整第139期链接
    - 新增第139期周刊内容文档
    - 更新最新期刊内容为第140期

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -6,7 +6,8 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
             text: '2026 年刊',
             collapsed: true,
             items: [
-                { text: '第 139 期', link: '/2026/latest' },
+                { text: '第 140 期', link: '/2026/latest' },
+                { text: '第 139 期', link: '/2026/0110-0116' },
                 { text: '第 138 期', link: '/2026/0103-0109' },
             ]
         }
